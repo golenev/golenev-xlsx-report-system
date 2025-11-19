e82a3f0f5e23d52b9433ed43c604c0bec5b072f0
Голенев Василий <84588235+golenev@users.noreply.github.com>
2025-11-19 12:12:50 +0300

    Point Allure test to docker backend

--- backend/src/test/kotlin/com/example/report/tests/AllureTestCasesIntegrationTest.kt
+++ backend/src/test/kotlin/com/example/report/tests/AllureTestCasesIntegrationTest.kt
@@ -11,26 +11,18 @@ import io.restassured.module.kotlin.extensions.Then
 import io.restassured.module.kotlin.extensions.When
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import java.nio.file.Paths
-
 class AllureTestCasesIntegrationTest {
 
     private val mapper = jacksonObjectMapper()
     private val baseUrl: String = System.getenv("ALLURE_API_BASE_URL")
         ?: System.getProperty("allure.api.base-url")
-        ?: "http://localhost:8080"
+        ?: "http://localhost:18080"
 
     @Test
     fun `should return parsed allure test cases`() {
         val folderPath = System.getenv("ALLURE_TEST_CASES_PATH")
             ?: System.getProperty("allure.test-cases.path")
-            ?: Paths.get(
-                "src",
-                "test",
-                "resources",
-                "allure",
-                "test-cases"
-            ).toAbsolutePath().toString()
+            ?: "C:\\Users\\inter\\IdeaProjects\\motivation-service-tests\\build\\reports\\allure-report\\allureReport\\data"
 
         val response = Given {
             baseUri(baseUrl)
