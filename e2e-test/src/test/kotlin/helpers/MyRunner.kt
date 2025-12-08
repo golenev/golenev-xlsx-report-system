d771e05aa643b8980ec4d67dedbd78569a40761b
golenev.vasiliy <golenev.vasiliy@rwb.ru>
2025-12-08 19:25:01 +0300

    Добавление отправка собственных тестов в репорт портал

---
+++ e2e-test/src/test/kotlin/helpers/MyRunner.kt
@@ -0,0 +1,49 @@
+package helpers
+
+import io.restassured.RestAssured
+import io.restassured.http.ContentType
+import kotlin.system.exitProcess
+
+
+object MyRunner {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println(">>> Start MyRunner")
+        try {
+            val testCasesPath = "e2e-test/build/reports/allure-report/allureReport/data/test-cases"
+
+            println(">>> Start MyRunner")
+            val items = parseAllureReportsFromFolder(testCasesPath)
+            println(">>> Parsed test cases: ${items.size}")
+
+            RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(
+                    BatchRequest(
+                        items = items.map { testCase ->
+                            TestCaseItem(
+                                testId = testCase.id,
+                                category = testCase.category,
+                                shortTitle = testCase.name,
+                                issueLink = "",
+                                readyDate = "",
+                                generalStatus = "Готово",
+                                scenario = testCase.scenario,
+                                notes = "",
+                                priority = "Medium"
+                            )
+                        }
+                    )
+                )
+                .log().all()
+                .post("http://localhost:18080/api/tests/batch")
+                .then()
+                .log().all()
+                .statusCode(200)
+            println(">>> Finished successfully")
+        } catch (e: Exception) {
+            e.printStackTrace()
+            exitProcess(12074)
+        }
+    }
+}
---
+++ e2e-test/src/test/kotlin/helpers/TestCaseModel.kt
@@ -0,0 +1,37 @@
+package helpers
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class BatchRequest(
+    @JsonProperty("items")
+    val items: List<TestCaseItem>,
+)
+
+data class TestCaseItem(
+    @JsonProperty("testId")
+    val testId: String,
+
+    @JsonProperty("category")
+    val category: String,
+
+    @JsonProperty("shortTitle")
+    val shortTitle: String,
+
+    @JsonProperty("issueLink")
+    val issueLink: String,
+
+    @JsonProperty("readyDate")
+    val readyDate: String,
+
+    @JsonProperty("generalStatus")
+    val generalStatus: String,
+
+    @JsonProperty("scenario")
+    val scenario: String,
+
+    @JsonProperty("notes")
+    val notes: String,
+
+    @JsonProperty("priority")
+    val priority: String,
+)
